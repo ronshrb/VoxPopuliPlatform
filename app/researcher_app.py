e6a93907fd8c0753676d9f16638b3afe8565dd06
ronshrb <ronshrb@gmail.com>
2025-06-11 07:11:01 +0300

    Fixed Word Cloud

--- app/researcher_app.py
+++ app/researcher_app.py
@@ -140,7 +140,7 @@ def researcher_app(userid, tables_dict):
             # --- Word Cloud ---
             st.subheader("Word Cloud")
             # Specify a font that supports Hebrew (update the path as needed)
-            font_path = "app\ARIAL.TTF"
+            font_path = r"app/ARIAL.TTF"
             def fix_hebrew(text):
                 # Reverse each Hebrew word
                 def reverse_hebrew_word(match):
